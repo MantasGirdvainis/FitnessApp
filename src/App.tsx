8d263414259c8a357b9efe2cb8585ed1a80b9027
Mantas Girdvainis <mantas.girdvainis@outlook.com>
2023-03-18 14:09:40 +0200

    home page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from '@/components/Navbar/Navbar';
 import { useEffect, useState } from 'react';
-import { SelectedPage } from './shared/types';
+import { SelectedPage } from '@/shared/types';
+import { Home } from '@/components/Home/Home';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
   return (
     <div className="app bg-gray-20">
       <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   )
 }
